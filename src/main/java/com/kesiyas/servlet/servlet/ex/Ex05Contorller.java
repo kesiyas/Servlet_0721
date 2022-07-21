17f9e0557e268b7c4d7ffabb9489f1f10b903079
303-12\user <93star2@naver.com>
2022-07-21 18:45:19 +0900

    [정민우] - post method 예제 ex05 클래스를 추가

---
+++ src/main/java/com/kesiyas/servlet/servlet/ex/Ex05Contorller.java
@@ -0,0 +1,19 @@
+package com.kesiyas.servlet.servlet.ex;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class Ex05Contorller extends HttpServlet{
+
+	@Override
+	public void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException {
+		response.setCharacterEncoding("utf-8");
+		response.setContentType("text/html");
+		
+		PrintWriter out = response.getWriter();
+	}
+}
